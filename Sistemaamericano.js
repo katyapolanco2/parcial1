86524322ef75b3383cd28426e8231d24ac977aef
katyapolanco2 <60717096+katyapolanco2@users.noreply.github.com>
2020-02-10 16:37:01 -0600

    Sistema americano
    Numero comparado con el sistema americano.

---
+++ Sistemaamericano.js
@@ -0,0 +1,14 @@
+var num1=Math.floor(Math.random()*10)+1;
+
+if (num1==10)
+    console.log(num1+ " A")
+else if(num1==9)
+    console.log(num1+" B")
+else if(num1==8)
+    console.log (num1+ " C")
+else if(num1==7)
+    console.log (num1 + " D")
+else if(num1==6)
+    console.log (num1+ " E")
+else 
+    console.log(num1+ " F")
