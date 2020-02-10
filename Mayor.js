c730358778c176bd6ab76689a27d585d1e67ef57
katyapolanco2 <60717096+katyapolanco2@users.noreply.github.com>
2020-02-10 16:36:14 -0600

    Numero mayor
    Numero mayor aleatorio

---
+++ Mayor.js
@@ -0,0 +1,13 @@
+var a=Math.floor(Math.random()*20);
+var b=Math.floor(Math.random()*20);
+var c=Math.floor(Math.random()*20);
+
+console.log (a,b,c);
+if(a>b && a>c)
+console.log(a+" Es mayor");
+if (b>a && b>c)
+console.log (b+" Es el numero mayor");
+if (c>a && c>b)
+console.log (c+ " Es el mayor");
+
+
